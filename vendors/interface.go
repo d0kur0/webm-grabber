587d9a8e77e525e7eac436bbcca99ae293445be4
d0kur0 <dev.dokuro@gmail.com>
2019-10-25 01:19:59 +0300

    Remove private declaration functions

--- vendors/interface.go
+++ vendors/interface.go
@@ -1,8 +1,6 @@
 package vendors
 
 type Interface interface {
-	execRequest(uri string) string
-	buildUri(uri string) string
 	FetchThreads(boardName string) map[int]string
 	FetchVideos(threadId int) map[int]string
 }
