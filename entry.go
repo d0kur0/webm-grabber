7c5a271b88df06dd4ed6e66ebb91b140c858c85a
Your Name <you@example.com>
2019-10-24 17:59:51 +0300

    Split code to packages

--- entry.go
+++ entry.go
@@ -1,50 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"strings"
-)
-
-type VendorInterface interface {
-	execRequest(uri string) string
-	buildUri(uri string) string
-	FetchThreads(boardName string) map[int]string
-	FetchVideos(threadId int) map[int]string
-}
-
-type VendorImplementation struct {
-	address string
-}
-
-func (vendor *VendorImplementation) buildUri(uri string) string {
-	return vendor.address + "/" + strings.Trim(uri, "/")
-}
-
-func (vendor *VendorImplementation) execRequest(uri string) string {
-	uri = vendor.buildUri(uri)
-
-	response, error := http.Get(uri)
-	if error != nil {
-		// Fuck!
-	}
-
-	fmt.Printf("%v", response)
-}
-
-func (vendor *VendorImplementation) FetchThreads(boardName string) map[int]string {
-	return map[int]string{}
-}
-
-func (vendor *VendorImplementation) FetchVideos(threadId int) map[int]string {
-	return map[int]string{}
-}
-
-func vendorFactory() VendorInterface {
-	return &VendorImplementation{address: "http://example.com"}
-}
+import "daemon/vendors/implementation"
 
 func main() {
-	vendor := vendorFactory()
-	_ = vendor
+	testVendor := implementation.TestVendorFactory()
+	_ = testVendor
 }
