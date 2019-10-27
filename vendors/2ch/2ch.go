a3d932ec74ebd038ff384058063e71c0100b18c7
d0kur0 <dev.dokuro@gmail.com>
2019-10-28 00:37:23 +0300

    Change return types, add parse threads response and return int slice

--- vendors/2ch/2ch.go
+++ vendors/2ch/2ch.go
@@ -1,28 +1,40 @@
-package implementation
+package _2ch
 
 import (
 	"daemon/vendors"
 	"encoding/json"
-
-	"github.com/davecgh/go-spew/spew"
+	"strconv"
 )
 
 type vendor struct {
 	request *vendors.Request
 }
 
-func (v *vendor) FetchThreads(boardName string) (response map[int]string, err error) {
+func (v *vendor) FetchThreads(boardName string) (threads []int, err error) {
 	jsonData, err := v.request.Exec(boardName + "/catalog_num.json")
 	if err != nil {
 		return
 	}
 
-	var result map[string]interface{}
-	json.Unmarshal(jsonData, &result)
+	var ThreadsStruct struct {
+		Threads []struct {
+			Id string `json:"num"`
+		}
+	}
 
-	threads := result["threads"].(map[string]interface{})
+	err = json.Unmarshal(jsonData, &ThreadsStruct)
+	if err != nil {
+		return
+	}
 
-	spew.Dump(threads)
+	for _, thread := range ThreadsStruct.Threads {
+		threadId, err := strconv.ParseInt(thread.Id, 10, 64)
+		if err != nil {
+			continue
+		}
+
+		threads = append(threads, int(threadId))
+	}
 
 	return
 }
@@ -31,7 +43,7 @@ func (v *vendor) FetchVideos(threadId int) (response map[int]string, err error) 
 	return
 }
 
-func Instance2ch() vendors.Interface {
+func Instance() vendors.Interface {
 	return &vendor{
 		vendors.RequestFactory("http://2ch.hk"),
 	}
