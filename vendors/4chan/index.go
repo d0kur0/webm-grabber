3355339ac15af609b083a2bc180d2c4af9512845
d0kur0 <dev.dokuro@gmail.com>
2019-10-30 13:29:07 +0300

    Add 4chan vendor implementation

---
+++ vendors/4chan/index.go
@@ -0,0 +1,76 @@
+package _4chan
+
+import (
+	"daemon/functions"
+	"daemon/structs"
+	"daemon/vendors"
+	"encoding/json"
+	"strconv"
+)
+
+type vendor struct {
+	request *vendors.Request
+}
+
+func (v *vendor) FetchThreads(boardName string) (threads []int, err error) {
+	jsonData, err := v.request.Exec(boardName + "/" + "threads.json")
+	if err != nil {
+		return
+	}
+
+	var ThreadsStruct []struct {
+		Threads []struct {
+			Id int `json:"no"`
+		}
+	}
+
+	if err = json.Unmarshal(jsonData, &ThreadsStruct); err != nil {
+		return
+	}
+
+	for _, threadPage := range ThreadsStruct {
+		for _, thread := range threadPage.Threads {
+			threads = append(threads, thread.Id)
+		}
+	}
+
+	return
+}
+
+func (v *vendor) FetchVideos(boardName string, threadId int) (videos []structs.Video, err error) {
+	jsonData, err := v.request.Exec(boardName + "/" + "thread" + "/" + strconv.Itoa(threadId) + ".json")
+	if err != nil {
+		return
+	}
+
+	var PostsStruct struct {
+		Posts []struct {
+			Filename      string
+			FileExtension string
+		}
+	}
+
+	err = json.Unmarshal(jsonData, &PostsStruct)
+	if err != nil {
+		return
+	}
+
+	for _, post := range PostsStruct.Posts {
+		if exists, _ := functions.InArray(post.FileExtension, vendors.AllowFileTypes); exists {
+			videos = append(videos, structs.Video{
+				ThreadId: threadId,
+				Path:     v.request.BuildUri("https://i.4cdn.org/" + boardName + "/" + post.Filename + post.FileExtension),
+				Name:     post.Filename,
+				Preview:  v.request.BuildUri("https://i.4cdn.org/" + boardName + "/" + post.Filename + "s" + post.FileExtension),
+			})
+		}
+	}
+
+	return
+}
+
+func Instance() vendors.Interface {
+	return &vendor{
+		vendors.RequestFactory("https://a.4cdn.org"),
+	}
+}
