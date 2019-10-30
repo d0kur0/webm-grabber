aa72b3d35fee13868dcd6664e340d68de30e439e
d0kur0 <dev.dokuro@gmail.com>
2019-10-30 13:25:21 +0300

    Rename to index

--- vendors/2ch/index.go
+++ vendors/2ch/index.go
@@ -1,9 +1,11 @@
 package _2ch
 
 import (
+	"daemon/functions"
 	"daemon/structs"
 	"daemon/vendors"
 	"encoding/json"
+	"path/filepath"
 	"strconv"
 )
 
@@ -12,7 +14,7 @@ type vendor struct {
 }
 
 func (v *vendor) FetchThreads(boardName string) (threads []int, err error) {
-	jsonData, err := v.request.Exec(boardName + "/catalog_num.json")
+	jsonData, err := v.request.Exec(boardName + "/threads.json")
 	if err != nil {
 		return
 	}
@@ -41,17 +43,43 @@ func (v *vendor) FetchThreads(boardName string) (threads []int, err error) {
 }
 
 func (v *vendor) FetchVideos(boardName string, threadId int) (videos []structs.Video, err error) {
-	jsonData, err := v.request.Exec(boardName + "/res/" + string(threadId) + ".json")
+	jsonData, err := v.request.Exec(boardName + "/res/" + strconv.Itoa(threadId) + ".json")
 	if err != nil {
 		return
 	}
 
 	var PostsStruct struct {
+		Threads []struct {
+			Posts []struct {
+				Files []struct {
+					Name    string `json:"fullname"`
+					Path    string `json:"path"`
+					Preview string `json:"thumbnail"`
+				}
+			}
+		}
 	}
 
-	err = json.Unmarshal(jsonData, PostsStruct)
+	err = json.Unmarshal(jsonData, &PostsStruct)
 	if err != nil {
 		return
+	}
+
+	for _, post := range PostsStruct.Threads[0].Posts {
+		if post.Files == nil {
+			continue
+		}
+
+		for _, file := range post.Files {
+			if exists, _ := functions.InArray(filepath.Ext(file.Path), AllowFileTypes); exists {
+				videos = append(videos, structs.Video{
+					ThreadId: threadId,
+					Path:     file.Path,
+					Name:     file.Name,
+					Preview:  file.Preview,
+				})
+			}
+		}
 	}
 
 	return
