bf4855738596908c4845a0d6b3f9edb044ef161f
d0kur0 <dev.dokuro@gmail.com>
2020-01-10 17:21:04 +0300

    Test for fetchTheads of twoChannel package completed

--- vendors/twoChannel/Implement_test.go
+++ vendors/twoChannel/Implement_test.go
@@ -5,9 +5,15 @@ import (
 	"testing"
 )
 
-var implement = Make(types.AllowedExtensions{".webm", ".mp4"})
+var testImplement = Make(types.AllowedExtensions{".webm", ".mp4"})
 
 func TestFetchThreads(t *testing.T) {
-	threads, err := implement.FetchThreads("b")
+	threads, err := testImplement.FetchThreads("b")
+	if err != nil {
+		t.Error("Fetch threads error", err)
+	}
 
+	if len(threads) == 0 {
+		t.Error("FetchingThreads retun a 0 rows")
+	}
 }
