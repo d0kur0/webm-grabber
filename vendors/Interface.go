68d4dd44faf7e6e2504531e09b41ed632eca2b6f
d0kur0 <dev.dokuro@gmail.com>
2020-01-13 03:45:50 +0300

    Add function VendorName

--- vendors/Interface.go
+++ vendors/Interface.go
@@ -5,4 +5,5 @@ import "daemon/types"
 type Interface interface {
 	FetchThreads(board types.Board) (threads []types.Thread, err error)
 	FetchFiles(thread types.Thread) (files []types.File, err error)
+	VendorName() string
 }
